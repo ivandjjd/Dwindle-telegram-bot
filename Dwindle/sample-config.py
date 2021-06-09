7e3e3c5280698602d87d506ae36ffcf4332d4a0e
vacantinside <85221675+ivandjjd@users.noreply.github.com>
2021-06-08 20:44:05 -0700

    Update sample-config.py

--- Dwindle/sample-config.py
+++ Dwindle/sample-config.py
@@ -6,9 +6,9 @@ if not __name__.endswith("sample_config"):
 
 # Create a new config.py file in same dir and import, then extend this class.
 
-TOKEN = "" # Your bot Token here
-Webhook = "" #webhook url here.(Ex: https://dwindle.heroku.app.If you are using heroku get using the format - https://{your heroku app name here}.herokuapp.com/)
-GpApi = "" #Get this at https://bitly.is/accesstoken
-GpBase = " https://gplinks.in/api?api={}&url=".format(GpApi)
-bitlyapi = "" #Get this at https://bitly.is/accesstoken
-bitlybase = "https://api-ssl.bitly.com/v3/shorten?access_token={}&uri=".format(bitlyapi)
+TOKEN       =     ""    # Your bot Token here
+Webhook     =     ""    #webhook url here.(Ex: https://dwindle.heroku.app.If you are using heroku get using the format - https://{your heroku app name here}.herokuapp.com/)
+GpApi       =     ""    #Get this at https://bitly.is/accesstoken
+GpBase      =     "https://gplinks.in/api?api={}&url=".format(GpApi)
+bitlyapi    =     ""    #Get this at https://bitly.is/accesstoken
+bitlybase   =     "https://api-ssl.bitly.com/v3/shorten?access_token={}&uri=".format(bitlyapi)
